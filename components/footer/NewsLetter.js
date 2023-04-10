1adda4759ef77cabed02c82fa7953481d748f52f
Mahmoud Salman <salmanmahmoud70@gmail.com>
2023-04-11 01:06:39 +0200

    newsletter section in footer has been completed

---
+++ components/footer/NewsLetter.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import styles from './styles.module.scss'
+const NewsLetter = () => {
+  return (
+    <div className={styles.footer__news__letter}>
+      <h3>SIGN UP FOR OUR NewsLetter</h3>
+      <div className={styles.footer__flex}>
+        <input type="email" placeholder="Your Email Address..." />
+        <button className={styles.btn_primary}>SUBSCRIBE</button>
+      </div>
+
+      <p>
+        By clicking the SUBSCRIBE button, you are agreeing to{' '}
+        <Link href="">ourPrivacy & Cookie Policy</Link>
+      </p>
+    </div>
+  )
+}
+
+export default NewsLetter
--- components/footer/index.js
+++ components/footer/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './styles.module.scss'
 import Links from './Links'
 import Socials from './Socials'
+import NewsLetter from './NewsLetter'
 
 const Footer = () => {
   return (
@@ -9,6 +10,7 @@ const Footer = () => {
       <div className={styles.footer__container}>
         <Links />
         <Socials />
+        <NewsLetter />
       </div>
     </footer>
   )
