a102381020a105042d90dc2b9ee3cebb74df6524
Mahmoud Salman <salmanmahmoud70@gmail.com>
2023-04-15 02:38:03 +0200

    github provider in next-auth has been completed

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from 'next-auth'
+// import AppleProvider from 'next-auth/providers/apple'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import GoogleProvider from 'next-auth/providers/google'
+// import EmailProvider from 'next-auth/providers/email'
+
+import GitHubProvider from 'next-auth/providers/github'
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.JWT_SECRET,
+  providers: [
+    // OAuth authentication providers...
+    GitHubProvider({
+      clientId: process.env.GitHub_ID,
+      clientSecret: process.env.GitHub_SECRET,
+    }),
+    // AppleProvider({
+    //   clientId: process.env.APPLE_ID,
+    //   clientSecret: process.env.APPLE_SECRET,
+    // }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+    // Passwordless / email sign in
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: 'NextAuth.js <no-reply@example.com>',
+    // }),
+  ],
+})
